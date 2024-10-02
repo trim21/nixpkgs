1a02b0d9658a909a22e514ea9b60ff2ea71436c1
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-10-03 01:34:24 +0200

    fixup! python3Packages.django-gravatar2: 1.4.4 -> 1.4.5

--- pkgs/development/python-modules/django-gravatar2/default.nix
+++ pkgs/development/python-modules/django-gravatar2/default.nix
@@ -2,19 +2,25 @@
   lib,
   buildPythonPackage,
   fetchPypi,
+  setuptools,
 }:
 
 buildPythonPackage rec {
   pname = "django-gravatar2";
   version = "1.4.5";
-  format = "setuptools";
+  pyproject = true;
 
   src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-LbtWRl45Xdizkg1AF+J6R1aRLMKtmxG6SM8UOHGoA2Q=";
+    pname = "django_gravatar2";
+    inherit version;
+    hash = "sha256-LbtWRl45Xdizkg1AF+J6R1aRLMKtmxG6SM8UOHGoA2Q=";
   };
 
+  build-system = [ setuptools ];
+
   doCheck = false;
+
+  pythonImportsCheck = [ "django_gravatar" ];
 
   meta = with lib; {
     description = "Essential Gravatar support for Django";
