026be88aac69e9954fe8c10fe0a93686db024075
natsukium <tomoya.otabi@gmail.com>
2024-10-07 17:31:49 +0900

    fixup! python3Packages.pywavelets: 1.6.0 -> 1.7.0

--- pkgs/development/python-modules/pywavelets/default.nix
+++ pkgs/development/python-modules/pywavelets/default.nix
@@ -25,7 +25,7 @@ buildPythonPackage rec {
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace-fail '"numpy>=2.0.0rc1,<2.3",' ""
+      --replace-fail "numpy>=2.0.0" "numpy"
   '';
 
   build-system = [
